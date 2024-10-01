1410f62b536405b46803bad9d2f044907b322b12
Roberto Cassio Soares Lino <rcassio@aiko.digital>
2024-10-01 14:58:39 -0300

    added menu

--- main.py
+++ main.py
@@ -6,36 +6,52 @@ import math
 #Todos eles são pontos de atenção
 #Se o id_projeto for diferente do recset[3] então adicionar um Atenção na última coluna, caso contrário deixar um OK
 # Problema em encontrar alguns shapes com a query, verificar
-project_id_column = input('Nome do Talhão no Shapefile:')
-wkt_conversions = shape_conversion.shapeConverter(project_id_column)
-intersection = intersection.Intersection(wkt_conversions)
 
-id_projeto = [wkt[0] for wkt in wkt_conversions]
 
-#GUI.MainScreen()
 
-count = 1
-print(wkt_conversions[0])
-for item in intersection:
-    rec_list = list(item)
-    rec_list[-3] = f"{math.trunc(rec_list[-3] * 100)}%"
-    rec_list[-2] = f"{math.trunc(rec_list[-2] * 100)}%"
+def show_menu():
+    print("\nMenu de Opções")
+    print("1. Verificar Talhões")
+    print("2. Sair")
 
-    if not rec_list[1] and not rec_list[2]:
-        rec_list.append("New")
-        print(rec_list[5:])
-        print(f'{count}/{len(intersection)}')
-    elif rec_list[-1] != rec_list[2]:
-        rec_list.append("Attention")
-        print(rec_list[1:])
-        print(f'{count}/{len(intersection)}')
+def verify_shapes():
+    count = 1
+    print(wkt_conversions[0])
+    for item in intersection:
+        rec_list = list(item)
+        rec_list[-3] = f"{math.trunc(rec_list[-3] * 100)}%"
+        rec_list[-2] = f"{math.trunc(rec_list[-2] * 100)}%"
+
+        if not rec_list[1] and not rec_list[2]:
+            rec_list.append("New")
+            print(rec_list[5:])
+            print(f'{count}/{len(intersection)}')
+        elif rec_list[-1] != rec_list[2]:
+            rec_list.append("Attention")
+            print(rec_list[1:])
+            print(f'{count}/{len(intersection)}')
+        else:
+            rec_list.append("OK")
+            print(rec_list[1:])
+            print(f'{count}/{len(intersection)}')
+
+        count += 1
+while True:
+    show_menu()
+    option = input("Escolha uma opção: ")
+
+    if option == '1':
+        project_id_column = input('Nome do Talhão no Shapefile:')
+        wkt_conversions = shape_conversion.shapeConverter(project_id_column)
+        intersection = intersection.Intersection(wkt_conversions)
+
+        id_projeto = [wkt[0] for wkt in wkt_conversions]
+        verify_shapes()
+    elif option == '2':
+        print("Saindo...")
+        break
     else:
-        rec_list.append("OK")
-        print(rec_list[1:])
-        print(f'{count}/{len(intersection)}')
-
-    count += 1
-
+        print("Opção inválida! Tente novamente.")
 
 
 
